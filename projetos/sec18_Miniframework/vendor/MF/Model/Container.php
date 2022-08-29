9c44a1454061f03ace393092c9dc182f99f77a3b
Nyaou <90991026+Nyaou@users.noreply.github.com>
2022-08-29 13:23:05 -0300

    Updt29_ago
    Seção 18 - Projeto Miniframework

---
+++ projetos/sec18_Miniframework/vendor/MF/Model/Container.php
@@ -0,0 +1,13 @@
+<?php 
+namespace MF\Model;
+use App\Connection;
+class Container{
+	public static function getModel($model){
+
+		$class = "\\App\\Model\\".ucfirst($model);
+		$conn = Connection::getDb();
+
+		return new $class($conn);
+	}
+}
+?>
